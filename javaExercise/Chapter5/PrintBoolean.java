36bcaae850563105660d2bea47948b7544868c4b
Baskervir <persona903@gmail.com>
2024-09-13 00:39:13 +0900

    add Chapter5/PrintBoolean

---
+++ javaExercise/Chapter5/PrintBoolean.java
@@ -0,0 +1,13 @@
+package Chapter5;
+/**
+ * 논리형 변수에 true나 false를 대입해서 표시합니다
+ * **/
+public class PrintBoolean {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println("b1 = " + b1);
+        System.out.println("b2 = " + b2);
+    }
+}
