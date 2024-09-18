20a96765cd8d4f927d3fae86a20d0a10f080ba14
Baskervir <persona903@gmail.com>
2024-09-18 23:21:56 +0900

    add Chapter7 linearSearch part

--- javaExercise/Chapter7/MinOfHeightWeight.java
+++ javaExercise/Chapter7/MinOfHeightWeight.java
@@ -35,3 +35,18 @@ public class MinOfHeightWeight {
         System.out.println("가장 가벼운 사람의 체중 : " + minOf(weight) + "kg");
     }
 }
+
+/**
+ * minOf 메서드는 'int형 배열(의 참조)을 받아서 int형의 값을 반환하는 구조'
+ * 
+ * -minOf(height)
+ * =신장용 배열 height의 참조를 minOf 메서드에 전달
+ * =minOf 내의 배열 변수 a는 height을 참조한다
+ * 
+ * -minOf(weight)
+ * =체중용 배열 weight의 참조를 minOf 메서드에 전달
+ * =minOf 냐의 배열 변수 a는 weight을 참조한다
+ * 
+ * 매개 변수의 형은 int[]이다
+ * 배열 요소의 형이 int형이면 받는 배열의 요소 수에는 제약이 없다
+ * **/
