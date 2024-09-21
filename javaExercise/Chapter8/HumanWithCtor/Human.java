757fa826d9638424d12c09440e21968cabd0b94e
Baskervir <persona903@gmail.com>
2024-09-22 00:19:24 +0900

    add Chapter8

---
+++ javaExercise/Chapter8/HumanWithCtor/Human.java
@@ -0,0 +1,33 @@
+package Chapter8.HumanWithCtor;
+
+public class Human {
+    private String name;
+    private int height;
+    private int weight;
+
+    public Human(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getHeight() {
+        return height;
+    }
+
+    int getWeight() {
+        return weight;
+    }
+
+    void gainWeight(int w) {
+        weight += w;
+    }
+
+    void loseWeight(int w) {
+        weight -= w;
+    }
+}
