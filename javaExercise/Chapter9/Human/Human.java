cfb0636833f2396c0e32f5043cc33cb53d523c97
Baskervir <persona903@gmail.com>
2024-09-26 07:54:02 +0900

    added Chapter9 Human

--- javaExercise/Chapter9/Human/Human.java
+++ javaExercise/Chapter9/Human/Human.java
@@ -1,14 +1,24 @@
 package Chapter9.Human;
 
+import Chapter9.Day.Day;
+
 public class Human {
     private String name;
     private int height;
     private int weight;
+    private Day birthday;
 
     public Human(String name, int height, int weight) {
         this.name = name;
         this.height = height;
         this.weight = weight;
+    }
+
+    public Human(String name, int height, int weight, Day birthday) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.birthday = new Day(birthday);
     }
 
     String getName() {
@@ -23,11 +33,23 @@ public class Human {
         return weight;
     }
 
+    Day getBirthday() { return new Day(birthday); }
+
     void gainWeight(int w) {
         weight += w;
     }
 
     void loseWeight(int w) {
         weight -= w;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", birthday=" + birthday +
+                '}';
     }
 }
