81fae669741237ce83addb60033244045a6b0a87
Baskervir <persona903@gmail.com>
2024-09-26 09:39:11 +0900

    add Chapter10 Human + context

---
+++ javaExercise/Chapter10/Human/Id.java
@@ -0,0 +1,30 @@
+package Chapter10.Human;
+
+public class Id {
+    static int counter = 0;
+    private int id;
+
+    public Id() {
+        id = ++counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+/**
+ * 변수를 비공개로 설정하는 것에 대하여
+ * 캡슐화 : OOP의 중요한 원칙 중 하나
+ * 외부에서 해당 변수에 직접 접근할 수 없도록 제한
+ *
+ * 이유와 의미
+ * 1. 데이터 보호
+ * - 비공개 변수 -> 외부 코드에서 변수 변경 불가 -> 보호 가능
+ * 2. 제어된 접근
+ * - private로 클래스 내부에서만 접근 가능 -> getter & setter로 외부에서 간접접근 허용
+ * 이를 통해 데이터의 유효성 검사 or 추가 로직 수행 가능
+ * 3. 유지 보수
+ * - 클래스 구현 변경 -> 외부 코드에 영향 X -> 내부 로직 수행 가능
+ * 
+ * **/
---
+++ javaExercise/Chapter10/Human/IdTester.java
@@ -0,0 +1,20 @@
+package Chapter10.Human;
+
+public class IdTester {
+    public static void main(String[] args) {
+        Id a = new Id();
+        Id b = new Id();
+
+        System.out.println("a의 식별 번호 : " + a.getId());
+        System.out.println("b의 식별 번호 : " + b.getId());
+
+        System.out.println("Id.counter = " + Id.counter);
+        System.out.println("a.counter = : " + a.counter);
+        System.out.println("b.counter = " + b.counter);
+    }
+}
+
+/**
+ * 클래스 변수 접근
+ *
+ * **/
