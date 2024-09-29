f923332c3a615dd72c4e2dcd8930c9a31c6ba88d
Baskervir <persona903@gmail.com>
2024-09-29 13:12:58 +0900

    add new Directory about Network Study

---
+++ javaExercise/Chapter12/Car/Car.java
@@ -0,0 +1,16 @@
+package Chapter12.Car;
+
+public class Car {
+    private String name;
+    private int width;
+    private int height;
+    private int length;
+    private double x;
+    private double y;
+    private double fuel;
+//    private Day purchaseDay;
+
+    Car(String name, int width, int height, int length, double fuel) {
+
+    }
+}
