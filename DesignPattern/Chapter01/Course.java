6503b09e8916392cfa927ccc1aa31e9d36d6f2d5
Baskervir <persona903@gmail.com>
2024-09-30 17:10:54 +0900

    start design pattern chapter01~

---
+++ DesignPattern/Chapter01/Course.java
@@ -0,0 +1,17 @@
+public class Course {
+    private String id;
+    private String name;
+    private int numOfStudents = 0;
+
+    public void addStudent() {
+        //수행문 기입란
+    }
+
+    public void deleteStudent() {
+        //수행문 기입란
+    }
+}
+
+/**
+ *
+ * **/
