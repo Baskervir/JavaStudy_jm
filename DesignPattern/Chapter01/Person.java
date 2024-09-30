e1ace6828eff50f5b9c49fbb86a4547d35669783
Baskervir <persona903@gmail.com>
2024-09-30 17:13:27 +0900

    add Person class

---
+++ DesignPattern/Chapter01/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    private Phone() phones;
+
+    public Person() {
+        phones = new Phone[2];
+    }
+
+    public Phone getPhone(int i) {
+        if (i == 0 || i == 1)
+            return phones[i];
+
+        return null;
+    }
+}
