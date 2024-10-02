255d41ba6abce34024d8e8d0ff31d3ac2192709f
Baskervir <persona903@gmail.com>
2024-10-02 18:54:23 +0900

    add Student class

--- DesignPattern/Chapter01/Student.java
+++ DesignPattern/Chapter01/Student.java
@@ -1,4 +1,35 @@
 public class Student {
     private Stirng name;
     private Vector<Course> courses;
+
+    public Student(String name) {
+        this.name = name;
+        courses = new Vector<Course>();
+    }
+
+    public void registerCourse(Course course) {
+        courses.add(course);
+    }
+
+    public void dropCourse(Course course) {
+        if (course.contains(course)) {
+            courses.remove(course);
+        }
+    }
+
+    public Vector<Course> getCourses() {
+        return courses;
+    }
+
+    public class Course {
+        private String name;
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
