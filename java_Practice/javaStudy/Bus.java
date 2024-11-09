2a88c4e24be36565a677935b592a700ec079b941
Baskervir <persona903@gmail.com>
2024-11-09 23:45:19 +0900

    practice about super-2

---
+++ java_Practice/javaStudy/Bus.java
@@ -0,0 +1,9 @@
+package javaStudy;
+
+public class Bus extends Car {
+    int fee;
+    public Bus(String name, int number, int fee) {
+        super(name, number);
+        this.fee = fee;
+    }
+}
