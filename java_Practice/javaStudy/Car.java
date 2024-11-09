992f35a096bf950bf1a66b6294994c42826283aa
Baskervir <persona903@gmail.com>
2024-11-09 21:59:19 +0900

    .

--- java_Practice/javaStudy/Car.java
+++ java_Practice/javaStudy/Car.java
@@ -30,6 +30,8 @@ public class Car {
     public void run(int speed) {
         System.out.println("차가 " + speed + " km로 달립니다.");
     }
+
+    
 }
 
 /**
---
+++ java_Practice/javaStudy/CarExam5.java
@@ -0,0 +1,10 @@
+package javaStudy;
+
+public class CarExam5 {
+    public static void main(String[] args) {
+        Car car = new Car("포니", 1234);
+
+        System.out.println("name: " + car.name);
+        System.out.println("number: " + car.number);
+    }
+}
