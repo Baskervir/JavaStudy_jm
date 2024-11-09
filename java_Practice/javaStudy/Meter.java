9bb569308be3042ff8cccab686cacf7c213e6c04
Baskervir <persona903@gmail.com>
2024-11-10 00:47:17 +0900

    .

--- java_Practice/javaStudy/Meter.java
+++ java_Practice/javaStudy/Meter.java
@@ -1,6 +1,10 @@
 package javaStudy;
 
 public interface Meter {
+    //public int BASE_FARE = 3000;
     public abstract void start();
     public abstract int stop(int distance);
 }
+/**
+ * 인터페이스에 정의한 변수는 상수
+ * **/
--- java_Practice/javaStudy/Taxi.java
+++ java_Practice/javaStudy/Taxi.java
@@ -1,11 +1,18 @@
 package javaStudy;
 
 public class Taxi implements Meter {
+
+    public int BASE_FARE = 3000;
+
     @Override
     public int stop(int distance) {
-        return distance * 2;
+        int fare = BASE_FARE + distance * 2;
+        System.out.println("운행을 종료합니다. 요금은 " + fare + "원입니다.");
+        return fare;
     }
 
     @Override
-    public void start() {}
+    public void start() {
+        System.out.println("운행을 시작합니다.");
+    }
 }
