fd2a9400e6098e5d95e8602a7c3fe67a177b10f6
Baskervir <persona903@gmail.com>
2024-11-09 23:40:25 +0900

    practice about super

---
+++ java_Practice/javaStudy/CarBasic.java
@@ -0,0 +1,8 @@
+package javaStudy;
+
+public class CarBasic {
+    String name;
+    public CarBasic(String name) {
+        System.out.println("Car의 " + name + " 생성자입니다.");
+    }
+}
---
+++ java_Practice/javaStudy/Truck.java
@@ -0,0 +1,8 @@
+package javaStudy;
+
+public class Truck extends CarBasic {
+    public Truck() {
+        super("Truck");
+        System.out.println("Truck의 기본 생성자입니다.");
+    }
+}
