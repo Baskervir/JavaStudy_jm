c4defee0242c6f1fc7a8f2a62ca745630c548668
Baskervir <persona903@gmail.com>
2024-11-09 21:46:11 +0900

    added 34

--- java_Practice/javaStudy/Car.java
+++ java_Practice/javaStudy/Car.java
@@ -11,6 +11,10 @@ public class Car {
     public void run() {
         System.out.println("차가 달립니다.");
     }
+
+    public void run(int speed) {
+        System.out.println("차가 " + speed + " km로 달립니다.");
+    }
 }
 
 /**
--- java_Practice/javaStudy/CarExam.java
+++ java_Practice/javaStudy/CarExam.java
@@ -3,6 +3,7 @@ package javaStudy;
 public class CarExam {
     public static void main(String[] args) {
         Car c = new Car();
-        c.print();
+        c.run();
+        c.run(100);
     }
 }
