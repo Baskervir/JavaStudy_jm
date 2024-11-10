9800b0cd640c270e6fc483bb9dce5e7efcd19706
Baskervir <persona903@gmail.com>
2024-11-10 22:35:09 +0900

    InnerExam & Action

---
+++ java_Practice/javaStudy/ActionExam.java
@@ -0,0 +1,14 @@
+package javaStudy;
+
+public class ActionExam {
+    public static void main(String[] args) {
+        Action action = new MyAction() {
+            @Override
+            public void exec() {
+                System.out.println("exec");
+            }
+        };
+
+        action.exec();
+    }
+}
---
+++ java_Practice/javaStudy/MyAction.java
@@ -0,0 +1,8 @@
+package javaStudy;
+
+public class MyAction extends Action {
+    @Override
+    public void exec() {
+        System.out.println("exec");
+    }
+}
