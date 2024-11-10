4c9cd9ce585e7f8397d888807338259c3dace64b
Baskervir <persona903@gmail.com>
2024-11-10 23:51:35 +0900

    added ExceptionExam2

---
+++ java_Practice/javaStudy/ExceptionoExam2.java
@@ -0,0 +1,14 @@
+package javaStudy;
+
+public class ExceptionoExam2 {
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        try {
+            array[20] = 5;
+        } catch (Exception e) {
+            System.out.println("배열의 범위를 지나쳤어요." + e.toString());
+        } finally {}
+        System.out.println("성공");
+    }
+}
