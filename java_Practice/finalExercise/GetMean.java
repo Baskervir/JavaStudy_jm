642c7369d4772efc3b8c9494164d97ed45f3136f
Baskervir <persona903@gmail.com>
2024-11-11 20:08:32 +0900

    added f-e #3

---
+++ java_Practice/finalExercise/GetMean.java
@@ -0,0 +1,17 @@
+package finalExercise;
+
+public class GetMean {
+    public int getMean(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+
+    public static void main(String[] args) {
+        int x[] = {5, 4, 3};
+        GetMean getMean = new GetMean();
+        System.out.println("평균값 : " + getMean.getMean(x));
+    }
+}
