6d66ac1430c6e0f3e8b17822848252bc507b036e
Baskervir <persona903@gmail.com>
2024-11-19 00:29:16 +0900

    added Question

--- java_basic/ch04/practiceExample/practiceExample7.java
+++ java_basic/ch04/practiceExample/practiceExample7.java
@@ -25,12 +25,14 @@ public class practiceExample7 {
             if (userInput.equals("1")) {
                 System.out.print("예금액>");
                 balance += sc.nextInt();
+                //개행문자? sc.nextLine(); 누락시 1 or 2 입력시에 (18~20) 라인이 재출력되는 문제
                 sc.nextLine();
             }
 
             if (userInput.equals("2")) {
                 System.out.print("출금액>");
                 balance -= sc.nextInt();
+                //개행문자? sc.nextLine(); 누락시 1 or 2 입력시에 (18~20) 라인이 재출력되는 문제
                 sc.nextLine();
             }
 
